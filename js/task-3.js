b13fd5c3b478cd71271d9309cc6b33741741fe85
Plane-Jumper5 <romariobelyaev@gmail.com>
2024-04-05 00:22:27 +0300

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,24 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-
-   return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+function filterArray(numbers, value) {
+    // Створення порожнього масиву для зберігання підходящих чисел
+    let filteredArray = [];
+    
+    // Цикл для ітерації кожного елемента масиву numbers
+    for (let i = 0; i < numbers.length; i++) {
+        // Перевірка, чи поточний елемент більший за значення value
+        if (numbers[i] > value) {
+            // Якщо так, додавання елемента до нового масиву
+            filteredArray.push(numbers[i]);
+        }
+    }
+    
+    // Повернення нового масиву з підходящими числами
+    return filteredArray;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
